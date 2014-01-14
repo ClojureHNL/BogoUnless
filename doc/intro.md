f5021fc6379042a8d54539ba3852d821240e3b9c
Kyle Oba <koba@mudphone.com>
2014-01-13 22:30:24 -1000

    Uh... bobounless macros.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to macroz
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/macroz/core.clj
@@ -0,0 +1,30 @@
+(ns macroz.core)
+
+;; Write a macro id which takes a function and a list of args: (id f a
+;; b c), and returns an expression which calls that function with the
+;; given args: (f a b c).
+(defmacro id [f & args]
+  `(~f ~@args))
+
+;; (unless true-or-false
+;;   (do this if false))
+(def x 5)
+(defmacro unless [pred x]
+  `(if-not ~pred ~x))
+
+(defn plus [x y]
+  (+ x y))
+
+(defmacro unless2 [pred x]
+  `(if ~pred nil ~x))
+
+(defmacro bogounless [pred x kick]
+  `(loop [cnt# 1]
+     (println "kick - count: " ~kick " - " cnt# )
+     (let [rand-bool# (< 0 (rand-int 2))
+           _# (println "rand-bool: " rand-bool#)]
+       (if (= cnt# 5)
+         nil
+         (if (= rand-bool# ~pred)
+           (recur (inc cnt#))
+           ~x)))))
